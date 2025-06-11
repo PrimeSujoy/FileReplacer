7cee377dd39c8bb39f1d276d78b9734a5ef67c39
Sujoy ॐ <iamsujoyp@gmail.com>
2025-06-11 20:22:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyromod import listen
 from config import Config
 from pyrogram import Client
 
-# the logging things
+# Logging setup
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -12,8 +12,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 DOWNLOAD_LOCATION = "/downloads"
 
-if __name__ == "__main__" :
-    
+if __name__ == "__main__":
     plugins = dict(
         root="plugins"
     )
@@ -24,6 +23,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-
 
     app.run()
