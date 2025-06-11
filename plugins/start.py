3abd7247566575eed7c0f98e86938fb91cfd0f6b
Sujoy ॐ <iamsujoyp@gmail.com>
2025-06-12 01:22:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,7 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-START_MSG = """*Hᴇʏ {}, Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ Fɪʟᴇ Rᴇᴘʟᴀᴄᴇ Bᴏᴛ!*
+START_MSG = """**Hᴇʏ {}, Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ Fɪʟᴇ Rᴇᴘʟᴀᴄᴇ Bᴏᴛ!**
 
 Yᴏᴜ ᴄᴀɴ ᴇᴀsɪʟʏ ʀᴇᴘʟᴀᴄᴇ ᴍᴇssᴀɢᴇs ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ɴᴇᴡ ғɪʟᴇs & ᴄᴀᴘᴛɪᴏɴs!
 
@@ -9,24 +9,31 @@ Hᴏᴡ ᴛᴏ ᴜsᴇ: 
 
 1. Reply to a file with /replace command
 
-2. Use /replace (link of the old message you want to replace)
+2. Use /replace {Link To Replace}.
 
 ✨ Powered by @SuperToppers"""
 
 HELP_MSG = """
-*Follow these steps*
+**Follow these steps**
 
 1. Reply to a file with /replace command.
 
-2. Use /replace (link of the old message you want to replace).
+2. Use /replace {Link To Replace}.
 
-*<u>Note</u>:- Both you & the bot must be an admin in the target channel.*"""
+**<u>Note</u>:- Both you & @ReplaceFilesBot must be an admin in the target channel.**"""
 
 @Client.on_message(filters.command('start') & filters.private)
 async def start(client, message):
     await message.reply_text(
         text=START_MSG.format(message.from_user.mention),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("JOIN CHANNEL 🚀", url="https://t.me/+eSL9_MHThgJiMmJl")]]),
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton('❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️', url='https://t.me/UncleChipssBot')],
+            [
+                InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/SuperToppers0'),
+                InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ɢʀᴏᴜᴘ', url='https://t.me/SuperToppers')
+            ],
+            [InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@SuperToppers')]
+        ]),
         quote=True
     )
 
